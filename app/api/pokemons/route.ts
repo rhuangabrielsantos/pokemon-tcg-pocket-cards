1ce6475f307ba43b129f3f0008efaa117508aab3
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2024-11-10 03:04:43 -0300

    fix filter

--- app/api/pokemons/route.ts
+++ app/api/pokemons/route.ts
@@ -15,25 +15,25 @@ export interface IPokemon {
 export async function GET(request: NextRequest) {
   const { searchParams } = request.nextUrl;
 
-  let data: IPokemon[] = [];
+  let data: IPokemon[] | undefined = [];
 
   try {
     const response = await fetch(
       "https://raw.githubusercontent.com/chase-manning/pokemon-tcg-pocket-cards/refs/heads/main/v1.json"
     );
-    const jsonData: IPokemon[] = await response.json();
+    const jsonData: IPokemon[] | undefined = await response.json();
     data = jsonData;
 
     if (searchParams.get("name") !== "undefined") {
       const name = searchParams.get("name")!.toLocaleLowerCase();
-      data = data.filter((pokemon) =>
+      data = data?.filter((pokemon) =>
         pokemon.name.toLowerCase().includes(name)
       );
     }
 
     if (searchParams.get("type") !== "undefined") {
       const type = searchParams.get("type")!.toLocaleLowerCase();
-      data = data.filter((pokemon) =>
+      data = data?.filter((pokemon) =>
         pokemon.type.toLowerCase().includes(type)
       );
     }
